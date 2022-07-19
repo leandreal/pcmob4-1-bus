e209e941298e13038dc35721ab52e37cb221cc45
Leandreal <leandrelim@gmail.com>
2022-07-20 00:44:14 +0800

    Add Styling to Bus App

--- App.js
+++ App.js
@@ -4,14 +4,23 @@ import {
   Text, 
   View, 
   TouchableOpacity, 
-  ActivityIndicator 
+  ActivityIndicator,
+  TextInput,
 } from "react-native";
-
+import { FontAwesome } from '@expo/vector-icons'; 
+//import { format } from "date-fns";//
+//import Moment from 'react-moment';//
+import moment from 'moment';
 
 
 export default function App() {
   const [loading, setLoading] = useState(true); 
   const [arrival, setArrival] = useState("");
+  const [busstop, setBusStop] = useState("");
+  const [busnumber, setBusNumber] = useState("");
+  
+
+
 
   const BUSSTOP_URL = "https://arrivelah2.busrouter.sg/?id=83139";
   
@@ -33,44 +42,117 @@ export default function App() {
     return ()=> clearInterval(interval);
   }, []);
   
+  
+
   return (
+  
     <View style={styles.container}>
-      <Text style={styles.title}>Bus Arrival Time: </Text>
+
+   
+
+    <FontAwesome name="bus" size={72} color="navy" />
+
+      <Text style={styles.title}>BUS ARRIVAL SG </Text>
+
+      <Text style={styles.header}>Your Bus Stop:</Text>
+
+      <TextInput
+        style={styles.input}
+        onChangeText={setBusStop}
+        value={busstop}
+        placeholder="Enter Bus Stop No. Here"
+        keyboardType="numeric"
+      />
+
+      <Text style={styles.header}>Your Bus Number:</Text>
       
-      <Text style={styles.arrivalTime}>{loading ? <ActivityIndicator color={"grey"}/> : arrival}</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={setBusNumber}
+        value={busnumber}
+        placeholder="Enter Bus No. Here"
+        keyboardType="numeric"
+        
+      />
+    
+      <Text style={styles.header}>Next Bus Arriving In: </Text>
+
+      <Text style={styles.arrivalTime}>
+          
+        {loading ? <ActivityIndicator color={"grey"}/>:
+        moment(arrival).format('YYYY MMM DD @ HH:MM')}
+        </Text>
       
+      <Text style={styles.header}>Subseq. Bus Arriving In: </Text>
+
+      <Text style={styles.arrivalTime}>
+     
+        {loading ? <ActivityIndicator color={"grey"}/>:
+        moment(arrival).format('YYYY MMM DD @ HH:MM')}
+
+      </Text>
+
       <TouchableOpacity style={styles.button} onPress={() => setLoading(true)}>
-        <Text style={styles.buttonText}>Refresh!</Text>
-        </TouchableOpacity>      
-      
+        <Text style={styles.buttonText}>Refresh</Text>
+      </TouchableOpacity>      
+    
+    <Text><FontAwesome name="copyright" size={12} color="black"/> Leandreal</Text>
+
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'orange',
     alignItems: 'center',
     justifyContent: 'center',
+
   },
 
   title: {
-    fontSize: 40,
+    fontSize: 32,
+    marginVertical: 10,
+    justifyContent:'center',
+    fontWeight: '600',
+    color: 'navy',
+  },
+
+  header: {
+    fontSize: 24,
     marginVertical: 20,
+    
   },
 
   button: {
-    backgroundColor: "teal",
+    backgroundColor: "orchid",
     padding: 20,
-    marginVertical: 20,
+    marginVertical: 50,
     borderRadius: 10,
+    borderWidth: 1,
   },
 
   buttonText: {
     fontSize: 20,
-    color: "white",
+    
   },
 
+  input: {
+    height: 50,
+    width: '70%',
+    margin: 2,
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    backgroundColor: 'white',
+  },
 
+  arrivalTime: {
+    fontSize: 15,
+    fontWeight: '500',
+    color: 'blue',
+    
+  },
 });
